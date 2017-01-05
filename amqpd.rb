d446d471feb649b2bbe6e0cf8d52fc592ff57b34
Lawrence Law <d201.c317@gmail.com>
2017-01-05 05:51:43 +0000

    Code Simplification

--- amqpd.rb
+++ amqpd.rb
@@ -23,7 +23,7 @@ class Rabbit
   # Core
   def start
     topic = @channel.topic('dns')
-    queue_in = @channel.queue('')
+    queue_in = @channel.queue('dns')
     @actions.each do |actions|
       queue_in.bind(topic, routing_key: actions)
     end
@@ -64,61 +64,61 @@ class Processor
     rabbit = Rabbit.new
     parsed = JSON.parse(body)
     puts ' [x] Task : ' + job_type
-    case job_type
-    when 'all'
-      msg = records.all
-    when 'search.name'
-      if !(parsed['payload'] =~ /^[a-zA-Z0-9][a-zA-Z0-9.-]{1,30}[a-zA-Z0-9]$/)
-        # Pre-check: is this domain right?
-        # http://stackoverflow.com/questions/10306690/domain-name-validation-with-regex
+    msg = case job_type
+          when 'all'
+            records.all
+          when 'search.name'
+            if !(parsed['payload'] =~ /^[a-zA-Z0-9][a-zA-Z0-9.-]{1,30}[a-zA-Z0-9]$/)
+              # Pre-check: is this domain right?
+              # http://stackoverflow.com/questions/10306690/domain-name-validation-with-regex
 
-        msg = { message: 'Invalid Domain', description: 'Domain rule incorrect.' }
-      else
-        msg = records.where(name: name)
-      end
-    when 'search.ip4'
-      if !IPAddress.valid_ipv4?(parsed['payload'])
-        msg = { message: 'Invalid Address', description: 'Not a valid IP address.' }
-      else
-        msg = records.where(ipv4address: parsed['payload'])
-      end
-    when 'search.ip6'
-      if !IPAddress.valid_ipv6?(ip)
-        msg = { message: 'Invalid Address', description: 'Not a valid IP address.' }
-      else
-        msg = records.where(ipv6address: IPAddress(parsed['payload']).to_s)
-      end
-    when 'search.id'
-      msg = records.first(id: parsed['payload'])
-    when 'update'
-      payload = parsed['payload']
+              { message: 'Invalid Domain', description: 'Domain rule incorrect.' }
+            else
+              records.where(name: name)
+            end
+          when 'search.ip4'
+            if !IPAddress.valid_ipv4?(parsed['payload'])
+              { message: 'Invalid Address', description: 'Not a valid IP address.' }
+            else
+              records.where(ipv4address: parsed['payload'])
+            end
+          when 'search.ip6'
+            if !IPAddress.valid_ipv6?(ip)
+              { message: 'Invalid Address', description: 'Not a valid IP address.' }
+            else
+              records.where(ipv6address: IPAddress(parsed['payload']).to_s)
+            end
+          when 'search.id'
+            records.first(id: parsed['payload'])
+          when 'update'
+            payload = parsed['payload']
 
-      if !payload.key?('id')
-        msg = { message: 'Invalid Request', description: 'Specify ID.' }
-      elsif (payload.key?('id') && payload['id'].empty?) || (payload.key?('ipv4address') && payload['ipv4address'].empty?) || (payload.key?('ipv6address') && payload['ipv6address'].empty?) || (payload.key?('name') && payload['name'].empty?)
-        msg = { message: 'Invalid Request', description: 'Empty Fields discovered.' }
-      elsif payload.key?('name') && !(payload['name'] =~ /^[a-zA-Z0-9][a-zA-Z0-9.-]{1,30}[a-zA-Z0-9]$/)
-        msg = { message: 'Invalid Domain', description: 'Domain rule incorrect.' }
-      elsif (payload.key?('ipv4address') && !IPAddress.valid_ipv4?(payload['ipv4address'])) || (payload.key?('ipv6address') && !IPAddress.valid_ipv6?(payload['ipv6address']))
-        msg = { message: 'Invalid Address', description: 'Not a valid IP address.' }
-      else
-        check_result = records.first(id: payload['id'])
+            if !payload.key?('id')
+              { message: 'Invalid Request', description: 'Specify ID.' }
+            elsif (payload.key?('id') && payload['id'].empty?) || (payload.key?('ipv4address') && payload['ipv4address'].empty?) || (payload.key?('ipv6address') && payload['ipv6address'].empty?) || (payload.key?('name') && payload['name'].empty?)
+              { message: 'Invalid Request', description: 'Empty Fields discovered.' }
+            elsif payload.key?('name') && !(payload['name'] =~ /^[a-zA-Z0-9][a-zA-Z0-9.-]{1,30}[a-zA-Z0-9]$/)
+              { message: 'Invalid Domain', description: 'Domain rule incorrect.' }
+            elsif (payload.key?('ipv4address') && !IPAddress.valid_ipv4?(payload['ipv4address'])) || (payload.key?('ipv6address') && !IPAddress.valid_ipv6?(payload['ipv6address']))
+              { message: 'Invalid Address', description: 'Not a valid IP address.' }
+            else
+              check_result = records.first(id: payload['id'])
 
-        if check_result.empty?
-          msg = { message: 'Not Found', description: 'The requested entry not found.' }
-        else
-          name = payload.key?('name') ? payload['name'] : check_result['name']
-          ipv4 = payload.key?('ipv4address') ? payload['ipv4address'] : check_result['ipv4address']
-          ipv6 = payload.key?('ipv6address') ? IPAddress(payload['ipv6address']).to_s : check_result['ipv6address']
-          json check_result.update(name: name, ipv4address: ipv4, ipv6address: ipv6)
-        end
-      end
-    when 'delete'
-      records.where(id: parsed['payload']).delete
-      msg = { message: 'Done', description: 'Record has been deleted.' }
-    else
-      msg = { message: 'Ouch', description: 'Job is not defined.' }
-    end
+              if check_result.empty?
+                { message: 'Not Found', description: 'The requested entry not found.' }
+              else
+                name = payload.key?('name') ? payload['name'] : check_result['name']
+                ipv4 = payload.key?('ipv4address') ? payload['ipv4address'] : check_result['ipv4address']
+                ipv6 = payload.key?('ipv6address') ? IPAddress(payload['ipv6address']).to_s : check_result['ipv6address']
+                json check_result.update(name: name, ipv4address: ipv4, ipv6address: ipv6)
+              end
+            end
+          when 'delete'
+            records.where(id: parsed['payload']).delete
+            { message: 'Done', description: 'Record has been deleted.' }
+          else
+            { message: 'Ouch', description: 'Job is not defined.' }
+          end
     rabbit.publish(JSON.generate(msg), msg_id)
   end
 end

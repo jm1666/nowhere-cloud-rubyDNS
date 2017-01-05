2c4569e0f504679c5236120f2e46771a971bfdb4
Lawrence Law <d201.c317@gmail.com>
2017-01-05 04:30:10 +0000

    TTL and some code optimization

--- dnsd.rb
+++ dnsd.rb
@@ -9,52 +9,72 @@ require 'sequel'
 # Load Environment Variables.
 Dotenv.load
 
-# Class: DNS
+# Class: Core
 # The DNS Server Core
-class DNS
+class Core
+  # Used to reduce code redundancy
+  IN = Resolv::DNS::Resource::IN
+  Name = Resolv::DNS::Name
+
+  # These are meant to be useless to put at initialize, so I moved them away
+  DNS_SUFFIX = ENV['DNS_SUFFIX'].empty? ? 'local' : ENV['DNS_SUFFIX']
+  DNS_BIND   = ENV['DNS_PORT'].empty? ? 5300 : ENV['DNS_PORT']
+  UPSTREAM_1 = ENV['UPSTREAM_DNS_IP1'].empty? ? '208.67.222.222' : ENV['UPSTREAM_DNS_IP1']
+  UPSTREAM_2 = ENV['UPSTREAM_DNS_IP2'].empty? ? '208.67.220.220' : ENV['UPSTREAM_DNS_IP2']
+  TTL_VALUE  = (ENV['DNS_TTL'].empty? ? 10 : ENV['DNS_TTL']).to_i
+
   # Confiure Binding and upstream DNS
   def initialize
-    @dnsbind = ENV['DNS_PORT'].empty? ? 5300 : ENV['DNS_PORT']
-    @dnssuffix = ENV['DNS_SUFFIX'].empty? ? 'local' : ENV['DNS_SUFFIX']
-    @upstreamdnsip1 = ENV['UPSTREAM_DNS_IP1'].empty? ? '208.67.222.222' : ENV['UPSTREAM_DNS_IP1']
-    @upstreamdnsip2 = ENV['UPSTREAM_DNS_IP2'].empty? ? '208.67.220.220' : ENV['UPSTREAM_DNS_IP2']
     @database = Sequel.connect(ENV['DATABASE_URL'])
   end
 
   # The real server instance...
   def start
+    # These "Just copy assignment" is not preventable ...
+
     records = @database[:records]
-    dnssuffix = @dnssuffix
-    esc_dnssuffix = Regexp.escape(@dnssuffix)
-    dnsupstream = RubyDNS::Resolver.new([[:udp, @upstreamdnsip1, 53], [:tcp, @upstreamdnsip1, 53], [:udp, @upstreamdnsip2, 53], [:tcp, @upstreamdnsip2, 53]])
-    RubyDNS.run_server(listen: [[:udp, '::', @dnsbind], [:tcp, '::', @dnsbind]], debug: false) do
+    esc_dnssuffix = Regexp.escape(DNS_SUFFIX)
+    upstreamdns = RubyDNS::Resolver.new([[:udp, UPSTREAM_1, 53], [:tcp, UPSTREAM_1, 53], [:udp, UPSTREAM_2, 53], [:tcp, UPSTREAM_2, 53]])
+    RubyDNS.run_server(listen: [[:udp, '::', DNS_BIND], [:tcp, '::', DNS_BIND]]) do
       # Catch Localhost Request
-      match(/localhost/, Resolv::DNS::Resource::IN::A) do |transaction|
+      match(/localhost/, IN::A) do |transaction|
         transaction.respond!('127.0.0.1')
       end
 
       # Catch Localhost Request, on IPv6
-      match(/localhost/, Resolv::DNS::Resource::IN::AAAA) do |transaction|
+      match(/localhost/, IN::AAAA) do |transaction|
         transaction.respond!('::1')
       end
 
       # This is used to match the DNS Suffix of the internal zone
-      match(/(.+)\.#{esc_dnssuffix}/, Resolv::DNS::Resource::IN::A) do |transaction, match_data|
+      match(/(.+)\.#{esc_dnssuffix}/, IN::A) do |transaction, match_data|
         answer = records.first(name: match_data[1])
-        answer.nil? ? transaction.fail!(:NXDomain) : transaction.respond!(answer[:ipv4address])
+        if answer.nil?
+          transaction.fail!(:NXDomain)
+        else
+          transaction.respond!(answer[:ipv4address], ttl: TTL_VALUE)
+        end
       end
 
-      match(/(.+)\.#{esc_dnssuffix}/, Resolv::DNS::Resource::IN::AAAA) do |transaction, match_data|
+      match(/(.+)\.#{esc_dnssuffix}/, IN::AAAA) do |transaction, match_data|
         answer = records.first(name: match_data[1])
-        answer.nil? ? transaction.fail!(:NXDomain) : transaction.respond!(answer[:ipv6address])
+        if answer.nil?
+          transaction.fail!(:NXDomain)
+        else
+          transaction.respond!(answer[:ipv6address], ttl: TTL_VALUE)
+        end
       end
 
       # Handling PTR Record
-      match(/(.+)\.in-addr.arpa/, Resolv::DNS::Resource::IN::PTR) do |transaction, match_data|
+      match(/(.+)\.in-addr.arpa/, IN::PTR) do |transaction, match_data|
         realip = match_data[1].split('.').reverse.join('.')
         if IPAddress.valid_ipv4?(realip)
           answer = records.first(ipv4address: realip)
-          answer.nil? ? transaction.passthrough!(dnsupstream) : transaction.respond!(Resolv::DNS::Name.create(answer[:name] + '.' + dnssuffix))
+          if answer.nil?
+            transaction.passthrough!(upstreamdns)
+          else
+            transaction.respond!(Name.create(answer[:name] + '.' + DNS_SUFFIX), ttl: TTL_VALUE)
+          end
         else
           # Refusing inappropiate requests, inappropiate IPv6 requests also goes here.
           transaction.fail!(:Refused)
@@ -62,13 +82,17 @@ class DNS
       end
 
       # Handling IPv6 PTR Record
-      match(/(.+)\.ip6.arpa/, Resolv::DNS::Resource::IN::PTR) do |transaction, match_data|
+      match(/(.+)\.ip6.arpa/, IN::PTR) do |transaction, match_data|
         incoming = match_data[1].split('.').reverse.join
         if incoming =~ /^[0-9a-fA-F]+$/
-          realip6 = IPAddress::IPv6.parse_hex(match_data[1].split('.').reverse.join).to_s
+          realip6 = IPAddress::IPv6.parse_hex(incoming).to_s
           realip4 = (IPAddress::IPv6::Mapped.new(realip6).mapped? ? '::FFFF:' + IPAddress::IPv6::Mapped.new(realip6).ipv4.address : '').to_s
           answer = records.first(ipv6address: [realip6, realip4])
-          answer.nil? ? transaction.passthrough!(dnsupstream) : transaction.respond!(Resolv::DNS::Name.create(answer[:name] + '.' + dnssuffix))
+          if answer.nil?
+            transaction.passthrough!(upstreamdns)
+          else
+            transaction.respond!(Name.create(answer[:name] + '.' + DNS_SUFFIX), ttl: TTL_VALUE)
+          end
         else
           transaction.fail!(:Refused)
         end
@@ -76,11 +100,11 @@ class DNS
 
       # Default DNS handler, forward outside address to upstream DNS
       otherwise do |transaction|
-        transaction.passthrough!(dnsupstream)
+        transaction.passthrough!(upstreamdns)
       end
     end
   end
 end
 
-dns = DNS.new
+dns = Core.new
 dns.start
